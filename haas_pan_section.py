494992db112c70ba5c9fcfbf70b0f53ac5e61877
Matt Rüpp <mattrupp@gmail.com>
2025-12-01 14:39:43 -0500

    Add a breakdown of Haas Pan Door section part number

---
+++ haas_pan_section.py
@@ -0,0 +1,27 @@
+# define the substring of a Pan section
+pan_part_number = {
+    'Section Type': [0,1],
+    'Section Height': [1,3],
+    'Section Construction': [3,4],
+    'Painted Color': [4,5],
+    'Section Thickness': [6,7],
+    'Outside Skin Material': [7,8],
+    'Outside Panel Treatment':[8,9],
+    'Outside Skin Grooving': [9,10],
+    'Inside Skin Material': [11,12],
+    'DC Stile Gauge': [12,13],
+    'Section Length': [14,18],
+    'Steel End Stile Option': [18,19],
+    'Backer/Stile Placement Option': [19,20],
+    'Window/Exhause Port/Vent Quantity': [21,23],
+    'Window/Exhause Port/Vent Coding': [23,26],
+    'Window Insert Options': [27,28],
+    'Glazing, Exhause Port & Vent Options': [28,29],
+    'Seal & Struts Attached Option': [29,30]
+}
+
+stile_gauge = {
+    '3': '0.033" (20 GA) Stile', 
+    '4': '0.044" (18 GA) Stile', 
+    '5': '0.055" (16 GA) Stile',
+    }
